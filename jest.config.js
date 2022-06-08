068e7184cf98c89a39a221856d8a8c0ae7b6157d
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2022-06-08 20:32:31 +0545

    setup jest for unit tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['__tests__/**/*.[jt]s?(x)'],
+}
