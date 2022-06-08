f1ae9cbb678b9e5ff3edd864f07d1a80663b2796
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2022-06-08 18:47:12 +0545

    refine example

--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,34 @@
 const Client = require('@sawjan/teamup-client')
 
-const { SubCalendar } = new Client({
+const { Events, SubCalendar } = new Client({
   url: 'https://api.teamup.com',
   calToken: process.env.CALENDAR_TOKEN,
   apiKey: process.env.API_KEY,
 })
 
-;(async function () {
-  const { data } = await SubCalendar.listSubCalendars()
-  console.log(data)
+;(async function getLeaves() {
+  // fetch id for leave calendar
+  let leaveCalId
+  await SubCalendar.listSubCalendars({ includeInactive: true }).then(
+    ({ data: { subcalendars } }) => {
+      for (const subCal of subcalendars) {
+        if (subCal.name === 'leave') {
+          leaveCalId = subCal.id
+          break
+        }
+      }
+    }
+  )
+
+  // get all events identified by leaveCalId
+  const {
+    data: { events },
+  } = await Events.listEvents({
+    startDate: '2022-06-06',
+    endDate: '2022-06-10',
+    subcalendarId: leaveCalId,
+  })
+
+  // leave events
+  console.log(events)
 })()
