b23c7a132ab24666dd374ce4830da9e1cf9a6e77
Saw-jan <saw.jan.grg3e@gmail.com>
2024-02-27 18:30:30 +0545

    chore: update example file

--- examples/example.v1.js
+++ examples/example.v1.js
@@ -1,19 +1,19 @@
 //
-// This example works with the latest released version
+// This example works with the version ^1
 //
 
-const Teamup = require('../lib')
+const Client = require('@sawjan/teamup-client')
 
-const client = new Teamup({
+const { Events, SubCalendar } = new Client({
   url: 'https://api.teamup.com',
-  calendarKey: process.env.CALENDAR_KEY,
-  teamupToken: process.env.TEAMUP_TOKEN,
+  calToken: process.env.CALENDAR_TOKEN,
+  apiKey: process.env.API_KEY,
 })
 
 ;(async function getLeaves() {
   // fetch id for leave calendar
   let leaveCalId
-  await client.SubCalendar.listSubCalendars()
+  await SubCalendar.listSubCalendars()
     .then(({ data: { subcalendars } }) => {
       for (const subCal of subcalendars) {
         if (subCal.name === 'leave') {
@@ -22,16 +22,16 @@ const client = new Teamup({
         }
       }
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err.response.data))
 
   if (leaveCalId) {
     // get all events identified by leaveCalId
-    await client.Events.listEvents({
+    await Events.listEvents({
       startDate: '2022-06-06',
       endDate: '2022-06-10',
       subcalendarId: [leaveCalId],
     })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err))
+      .then(({ data: { events } }) => console.log(events))
+      .catch((err) => console.log(err.response.data))
   }
 })()
